e4004116afa3626ba943089996c9d6c5c5437f63
Ross Mckay <ross.mckay1975@gmail.com>
2019-10-18 16:19:44 +0100

    vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,32 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>Flight Information Screen</h1>
+    <div class="main-container">
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { eventBus } from './main.js'
+
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data(){
+    return {
+      flights: [],
+      selectedFlight: null
+      }
+    },
+    components: {
+
+    },
+  mounted(){
+    fetch('https://kabrudle.edinburghairport.com/api/flights/all')
+    .then(res => res.json())
+    .then(flights => this.flights = flights)
+
   }
 }
-</script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+</script>
